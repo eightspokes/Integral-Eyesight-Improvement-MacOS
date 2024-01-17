97e391bbe7c8216fac3651147b67795081a1ba52
Roman Kozulia <romanmuni8@gmail.com>
2024-01-16 19:00:12 -0500

    Initial Commit

---
+++ Integral-Eyesight-Improvement-MacOS/Integral-Eyesight-Improvement-MacOS/Integral_Eyesight_Improvement_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Integral_Eyesight_Improvement_MacOSApp.swift
+//  Integral-Eyesight-Improvement-MacOS
+//
+//  Created by Roman Kozulia on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Integral_Eyesight_Improvement_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
